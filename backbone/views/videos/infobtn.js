c4dd91c8f1a1d3ed1d4698b0221c3633d43ce186
Paul Carvill <paul@paulcarvill.com>
2015-07-07 17:54:19 +0100

    using a template for the track info

--- backbone/views/videos/infobtn.js
+++ backbone/views/videos/infobtn.js
@@ -10,15 +10,4 @@ JuiceVCR.Views.InfoButtonView = Backbone.View.extend({
         myPlayer1.exitFullScreen();
       }
     }
-      // $('#info').toggle();
-      // $('#info').html(window.JuiceVCR.data[currentVideoIndex].id + "<br />" + 
-      //     window.JuiceVCR.data[currentVideoIndex].tracktitle + "<br />" + 
-      //     window.JuiceVCR.data[currentVideoIndex].album + "<br />" + 
-      //     window.JuiceVCR.data[currentVideoIndex].artist + "<br />" + 
-      //     window.JuiceVCR.data[currentVideoIndex].label + "<br />" + 
-      //     window.JuiceVCR.data[currentVideoIndex].publisher + "<br />" + 
-      //     window.JuiceVCR.data[currentVideoIndex].body + "<br />" + 
-      //     window.JuiceVCR.data[currentVideoIndex].tags
-      //   );
-      // }
 })
--- backbone/views/videos/nextbtn.js
+++ backbone/views/videos/nextbtn.js
@@ -4,12 +4,7 @@ JuiceVCR.Views.NextButtonView = Backbone.View.extend({
     },
 
     doclick: function(){
-      currentVideoIndex += 1;
-      if(currentVideoIndex >= window.JuiceVCR.data.length){
-      	currentVideoIndex = 0;
-      }
-      myPlayer1.src({ type: window.JuiceVCR.data[currentVideoIndex].videotype, src: window.JuiceVCR.data[currentVideoIndex].videourl });
-
+      skipVid();
       // TODO: change text of play/pauyse button to 'play' because we always play when you skip to another track
     }
 })
