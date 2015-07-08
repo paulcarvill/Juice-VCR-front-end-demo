a03d0af2a6ac6cc26ee0a7ad80158fa6f5e795d6
Paul Carvill <paul@paulcarvill.com>
2015-07-08 14:33:01 +0100

    adding two versions of overlay state - with bg and without

--- vjs.youtube.js
+++ vjs.youtube.js
@@ -809,7 +809,7 @@
           if (stopPlaying) {
             // Replace YouTube play button by our own
             if(!this.player_.options()['ytcontrols']) {
-              this.playerEl_.querySelectorAll('.vjs-poster')[0].style.display = 'block';
+              //this.playerEl_.querySelectorAll('.vjs-poster')[0].style.display = 'block';
               if(typeof this.player_.bigPlayButton !== 'undefined') {
                 this.player_.bigPlayButton.show();
               }
